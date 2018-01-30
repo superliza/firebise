d38ff3e54a0656c40bd8ce139923a966c78e4de7
superliza <eli_darko@hotmail.com>
2018-01-30 04:31:33 -0600

    intento

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCTOoLBrH2mKLPv-YGYQ-MfkaJZQFnHJHw",
+    authDomain: "mi-fabuloso-proyecto-892e8.firebaseapp.com",
+    databaseURL: "https://mi-fabuloso-proyecto-892e8.firebaseio.com",
+    projectId: "mi-fabuloso-proyecto-892e8",
+    storageBucket: "",
+    messagingSenderId: "1052783818660"
+  };
+  firebase.initializeApp(config);
