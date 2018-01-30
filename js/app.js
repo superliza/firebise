0b3edc8aedc6fad1ed8916294b53a5bb2e92bd60
superliza <eli_darko@hotmail.com>
2018-01-30 05:24:42 -0600

    avances de firebase

--- js/app.js
+++ js/app.js
@@ -8,3 +8,43 @@ var config = {
     messagingSenderId: "1052783818660"
   };
   firebase.initializeApp(config);
+
+  $('#button-google').click(function () {
+    authGoogle();
+  })
+
+  function authGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    authentication(provider);
+  }
+
+  function authentication(provider) {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(result);     
+      // ...
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      console.log(error);
+      
+      var errorCode = error.code;
+      console.log(errorCode);
+      
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      console.log(errorMessage);
+      
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      console.log(email);
+      
+      var credential = error.credential;
+      // ...
+      console.log(credential);
+      
+    });
+  }
